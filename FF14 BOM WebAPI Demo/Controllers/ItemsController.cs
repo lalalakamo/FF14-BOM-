a108eef2e0f18a03899ff9578853433d4174d17b
lalalakamo <fyjh1000223@gmail.com>
2026-05-02 21:13:26 +0800

    1.新增BOM複製功能，put方法route參數1參數2將參數1的pro_id BOM資料複製到參數2的pro_id BOM資料 2.新增ITEM 自動編碼功能，若POST方法中，BODY未給新編號會直接取該材料最大編號+1新增至資料庫

--- FF14 BOM WebAPI Demo/Controllers/ItemsController.cs
+++ FF14 BOM WebAPI Demo/Controllers/ItemsController.cs
@@ -1,6 +1,9 @@
 ﻿using FF14BOM.Dtos;
 using FF14BOM.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.SqlServer.Query.Internal;
+using Microsoft.Identity.Client.Extensibility;
+using Microsoft.JSInterop.Infrastructure;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -43,7 +46,8 @@ namespace FF14BOM.Controllers
             var result = _webContext.Item.Select(i => new ItemGetDto { 
                 Mtr_id = i.Mtr_id,
                 Mtr_type = i.Mtr_type,
-                Mtr_Name = i.Mtr_Name
+                Mtr_Name = i.Mtr_Name,
+                NPC_Sell = i.NPC_Sell
             })
             .Where(i => i.Mtr_id == id).FirstOrDefault();
             if (result == null) return NotFound($"沒有{ id }的材料資料");
@@ -53,47 +57,84 @@ namespace FF14BOM.Controllers
 
         // POST api/<ItemsController>
         [HttpPost]
-        public IActionResult Post([FromBody] List<ItemDto> itemDtosList)
+        public IActionResult Post([FromBody] List<ItemAddDto> itemDtosList)
         {
             if (itemDtosList == null) return BadRequest();
             string logmsg="";
             foreach (var dto in itemDtosList)
-            { 
-                var exist = _webContext.Item.FirstOrDefault(i => i.Mtr_id == dto.Mtr_id);
-                if (exist != null)
+            {
+                if (string.IsNullOrEmpty(dto.Mtr_id))
                 {
-                    if (exist.NPC_Sell != dto.NPC_Sell || exist.Mtr_Name != dto.Mtr_Name || exist.Mtr_type != dto.Mtr_type)
-                    {
-                        exist.NPC_Sell = dto.NPC_Sell;
-                        exist.Mtr_Name = dto.Mtr_Name;
-                        exist.Mtr_type = dto.Mtr_type;
-                        logmsg += $"更新{dto.Mtr_id}的材料資料成功\n";
+                    if(string.IsNullOrEmpty(dto.Mtr_type))
+                        return BadRequest("若未輸入材料編號，材料類別不能為空，無法自動產生編號");
+
+                    var search = _webContext.Item
+                        .Where(i => i.Mtr_type == dto.Mtr_type)
+                        .OrderByDescending(i => i.Mtr_id)
+                        .Select(i => i.Mtr_id)
+                        .FirstOrDefault();
+
+                    int nextNum = 1;
+
+                    if (search != null && search.Length > 1)
+                    { 
+                        if(int.TryParse(search.Substring(1),out int currentMax))
+                            nextNum = currentMax +1;
                     }
-                    else
-                    {
-                        logmsg += $"已經有{dto.Mtr_id}的材料資料，且內容相同，跳過\n";
-                    }
-                }
-                else
-                {
-                    _webContext.Item.Add(new Item
-                    {
-                        Mtr_id = dto.Mtr_id,
+
+                    string newId = $"{dto.Mtr_type}{nextNum.ToString("D4")}";
+
+                    _webContext.Item.Add(new Item {
+                        Mtr_id = newId,
                         Mtr_Name = dto.Mtr_Name,
                         Mtr_type = dto.Mtr_type,
                         NPC_Sell = dto.NPC_Sell
                     });
-                    logmsg += $"新增{dto.Mtr_id}的材料資料成功\n";
+                    logmsg += $"新增{newId}的材料資料成功\n";
+
+                }
+                else
+                {
+                    var exist = _webContext.Item.FirstOrDefault(i => i.Mtr_id == dto.Mtr_id);
+                    if (exist != null)
+                    {
+                        if (exist.NPC_Sell != dto.NPC_Sell || exist.Mtr_Name != dto.Mtr_Name || exist.Mtr_type != dto.Mtr_type)
+                        {
+                            exist.NPC_Sell = dto.NPC_Sell;
+                            exist.Mtr_Name = dto.Mtr_Name;
+                            exist.Mtr_type = dto.Mtr_type;
+                            logmsg += $"更新{dto.Mtr_id}的材料資料成功\n";
+                        }
+                        else
+                        {
+                            logmsg += $"已經有{dto.Mtr_id}的材料資料，且內容相同，跳過\n";
+                        }
+                    }
+                    else
+                    {
+                        _webContext.Item.Add(new Item
+                        {
+                            Mtr_id = dto.Mtr_id,
+                            Mtr_Name = dto.Mtr_Name,
+                            Mtr_type = dto.Mtr_type,
+                            NPC_Sell = dto.NPC_Sell
+                        });
+                        logmsg += $"新增{dto.Mtr_id}的材料資料成功\n";
+                    }
                 }
             }
             _webContext.SaveChanges();
             return Ok(logmsg);
         }
         // DELETE api/<ItemsController>/5
-        //[HttpDelete("{id}")]
-        //public IActionResult Delete(string id)
-        //{
-
-        //}
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            var delete = _webContext.Item.FirstOrDefault(i => i.Mtr_id == id);
+            if (delete == null) return NotFound($"沒有{id}的材料資料，無法刪除");
+            _webContext.Item.Remove(delete);
+            _webContext.SaveChanges();
+            return Ok($"刪除{id}的材料資料成功");
+        }
     }
 }
